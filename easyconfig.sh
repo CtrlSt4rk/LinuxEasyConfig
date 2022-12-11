177850be2a58e1e0ff29d15bce8ccf1b02bef709
Andre <56407521+CtrlSt4rk@users.noreply.github.com>
2022-12-11 15:29:43 -0300

    Add files via upload
    added main menu, an utility downloader for terminal apps and some ui tweeks

--- easyconfig.sh
+++ easyconfig.sh
@@ -1,11 +1,35 @@
+function mainMenu(){
+	clear
+	echo "Welcome, choose the utility or press Q to quit"
+	echo "[0] AutoMount"
+	echo "[1] Utilities Download"
+
+	read -s -n 1 input
+	if [[ $input = "0" ]]
+	then
+    	automountboot 
+    fi
+    if [[ $input = "1" ]]
+	then
+    	downloadUtilities 
+    fi
+    if [[ $input = "q" ]] || [[ $input = "Q" ]]
+	then
+    	exit
+    fi
+}
+
 function automountboot(){
 	#mounting on standard points (/mnt/partitionName)
 	clear
 	printf '%s\n' "AutoMount utility, made for 1 time use only!! Use it at your own risk"
-	printf '%s\n' "Press A to AutoMount and press R to revert it"
-	read -s -n 1 optionAutoMount
-
-	if [ $optionAutoMount == "a" ] || [ $optionAutoMount == "A" ]
+	printf '%s\n' "Press A to AutoMount, press R to revert it or press Q to quit"
+	read -s -n 1 inputAutoMount
+	if [[ $inputAutoMount = "q" ]] || [[ $inputAutoMount = "Q" ]]
+	then
+    	mainMenu 
+    fi
+	if [ $inputAutoMount == "a" ] || [ $inputAutoMount == "A" ]
 	then
 		clear
 		printf '%s\n' "These are the found partitions in your system"
@@ -34,7 +58,7 @@ function automountboot(){
 		       echo "Mounting failed!"
 		fi 
 	fi
-	if [ $optionAutoMount == "r" ] || [ $optionAutoMount == "R" ]
+	if [ $inputAutoMount == "r" ] || [ $inputAutoMount == "R" ]
 	then
 		cp /etc/fstab.backup /etc/fstab
 		if [  $? -eq 0 ]
@@ -44,8 +68,53 @@ function automountboot(){
 		       echo "Restore failed!"
 		fi
 	fi
+
+	echo "Press Q to quit and go back to the main menu"
+	read -s -n 1 inputAutoMountEnd
+	if [[ $inputAutoMountEnd = "q" ]] || [[ $inputAutoMountEnd = "Q" ]]
+	then
+    	mainMenu
+    fi
+
 }
 
+function downloadUtilities(){
+	apt update
+	while true
+	do
+		clear
+		printf '%s\n' "Utilities Installer using apt"
+		printf '%s\n\n' "Press Q to quit"
+		echo "[0] hwloc (lstopo)"
+		echo "[1] Htop"
+		echo "[2] VIM"
+		echo ""
+		echo "[U] Update via terminal"
 
+		read -s -n 1 inputUtil
 
-automountboot
+		if [[ $inputUtil = "0" ]]
+		then
+        	apt install hwloc
+        fi
+        if [[ $inputUtil = "1" ]]
+		then
+	        apt install htop
+        fi
+        if [[ $inputUtil = "2" ]]
+		then
+        	apt install vim
+        fi
+        if [[ $inputUtil = "u" ]] || [[ $inputUtil = "U" ]]
+		then
+       		sudo apt update && sudo apt upgrade
+        fi
+
+		if [[ $inputUtil = "q" ]] || [[ $inputUtil = "Q" ]]
+		then
+        	mainMenu 
+        fi
+    done
+}
+
+mainMenu
